fd6b36dc8e6b74fed198c16fa29eb33688c75de9
6767676767 <192328467+kRxZykRxZy@users.noreply.github.com>
2025-11-07 17:34:25 +0000

    Fix rendering not working

--- v2/src/main.js
+++ v2/src/main.js
@@ -2,11 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-// Get the root DOM node
 const rootElement = document.getElementById("root");
 
-// Create the React root
 const root = createRoot(rootElement);
 
-// Render the App component
-root.render(App);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
