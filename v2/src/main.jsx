4cd65fdb40d5504c7d7c3f096a1948c2e96b0ec2
6767676767 <192328467+kRxZykRxZy@users.noreply.github.com>
2025-11-07 17:56:57 +0000

    Update main.jsx

--- v2/src/main.jsx
+++ v2/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import MenuBar from "./components/menu-bar/menu-bar.jsx";
 
 const rootElement = document.getElementById("root");
 
@@ -8,6 +9,7 @@ const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
+    <MenuBar username={window.username} />
     <App />
   </React.StrictMode>
 );
