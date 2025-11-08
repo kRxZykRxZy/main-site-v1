9e5fe7b263ef60fe7d33b83d00c9ea740d400ea9
6767676767 <192328467+kRxZykRxZy@users.noreply.github.com>
2025-11-08 08:09:13 +0000

    Remvoe unused funcs

--- v2/src/pages/SiteAuth.jsx
+++ v2/src/pages/SiteAuth.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { initializeApp } from "firebase/app";
 import {
-  getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
