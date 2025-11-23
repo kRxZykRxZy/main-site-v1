0894df79212ae11bcc15b03b6f449ed207be83fc
6767676767 <192328467+kRxZykRxZy@users.noreply.github.com>
2025-11-23 11:41:12 +0000

    make admin a follower

--- v2/src/pages/SiteAuth.jsx
+++ v2/src/pages/SiteAuth.jsx
@@ -8,7 +8,7 @@ import {
   onAuthStateChanged
 } from "firebase/auth";
 import { auth, db } from "../firebaseConfig"; // db imported from config
-import { doc, setDoc, getDoc } from "firebase/firestore";
+import { doc, setDoc, getDoc, arrayUnion } from "firebase/firestore";
 import Spinner from "../components/spinner/workspace";
 
 const API_BASE = "https://sl-api-v1.onrender.com";
@@ -68,16 +68,23 @@ const SnapLabsDashboard = () => {
   const createFirestoreUser = async (username, uid) => {
     try {
       const userRef = doc(db, "users", uid);
+      const adminRef = doc(db, "users", "YzS99EK1RbeXniXxPhAXuuv5sk93");
       const docSnap = await getDoc(userRef);
+      const adminSnap = await getDoc(adminRef);
       if (!docSnap.exists()) {
         await setDoc(userRef, {
           username,
           uid,
-          followers: [""],
+          followers: ["Admin"],
           followings: ["Admin"],
           achievements: [""]
         });
         console.log("User document created in Firestore.");
+      }
+      if (!adminSnap.exists()) {
+        await updateDoc(adminRef, {
+          followings: arrayUnion(username)
+        });
       }
     } catch (err) {
       console.error("Failed to create Firestore user:", err);
