2b69e5f08a720164c702ac3bcf5c98787fbe4ee4
6767676767 <192328467+kRxZykRxZy@users.noreply.github.com>
2025-11-25 07:41:29 +0000

    Remove unnecessary objects

--- v2/vite.config.js
+++ v2/vite.config.js
@@ -2,36 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {
-    port: 3000,
-    open: true,
-  },
-
-  build: {
-    outDir: "dist",
-    sourcemap: true,
-
-    minify: false,               // Do NOT minify code
-    chunkSizeWarningLimit: 90,   // Warn for chunks > 90KB
-
-    rollupOptions: {
-      output: {
-        // Split every dependency into its own chunk
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            const parts = id.split("node_modules/")[1].split("/");
-            
-            // Handle scoped packages like @firebase/app
-            if (parts[0].startsWith("@")) {
-              return `${parts[0]}/${parts[1]}`;
-            }
-
-            return parts[0];
-          }
-        },
-      },
-    },
-  },
-
   plugins: [react()],
 });
